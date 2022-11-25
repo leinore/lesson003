de3ccc0cc23fb902f8d9ca6f078ca9633ad24042
leinore <116540768+leinore@users.noreply.github.com>
2022-11-25 14:26:11 +0300

    add method with text

--- less_Methods/Program.cs
+++ less_Methods/Program.cs
@@ -60,18 +60,78 @@ Console.WriteLine(year);
 
 
 // Вид4
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result +text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "qwerty");
+// Console.WriteLine(res);
+
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-
-    while (i < count)
+    for (int i = 0; i < count; i++)
     {
         result = result +text;
-        i++;
     }
     return result;
 }
 
 string res = Method4(10, "qwerty");
-Console.WriteLine(res);
+// Console.WriteLine(res);
+
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <=10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+// – Я думаю, – сказал князь, улыбаясь, – что, 
+// ежели бы вас послали вместо нашего милого Винценгероде, 
+// вы бы взяли приступом согласие прусского короля. 
+// Вы так красноречивы. Вы дадите мне чаю?
+
+string text = "– Я думаю, – сказал князь, улыбаясь, – что, "
+                + "ежели бы вас послали вместо нашего милого Винценгероде,"
+                + "вы бы взяли приступом согласие прусского короля."
+                + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             0123
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+int length = text.Length;
+for (int i = 0; i < length; i++)
+{
+    if (text[i] == oldValue) result = result + $"{newValue}";
+    else result = result + $"{text[i]}";
+}
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'с', 'С');
+Console.WriteLine(newText);
